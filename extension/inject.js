8a5aa406ab108200b7a150dff345589954994904
Carlos Cadena <cs.cadena@gmail.com>
2017-06-12 16:38:58 -0700

    working with response from server, bordering comments with appropriate colors

--- extension/inject.js
+++ extension/inject.js
@@ -4,14 +4,24 @@
   $grabFromPage.addClass(function(index){
     return 'reddex' + index;
   });
-  // console.log($grabFromPage)
   $grabFromPage.each(function(index){
-    textToEval[$grabFromPage[index].className] = $grabFromPage[index].innerText;
+    textToEval[$grabFromPage[index].className.split(' ')[1]] = $grabFromPage[index].innerText;
   });
   console.log(textToEval)
   $.post('https://reddex.herokuapp.com/inbound', textToEval, function(response){
     console.log(response)
+    for(key in response){
+      $('.' + key).addClass(function(index){
+        if(response[key] <= -.3){
+          return 'neg1'
+        }
+        else if(response[key] > 0.3){
+          return 'pos1'
+        }
+        else{
+          return 'neu'
+        }
+      })
+    }
   });
-  // document.body.style.backgroundColor="red";
-
 })();
