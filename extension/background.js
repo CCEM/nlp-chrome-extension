477c1e6efc24eab02e3fb518e9afd2511662b9b2
Morgan Nomura <morganelle@gmail.com>
2017-06-14 11:36:48 -0700

    added icon, trying out page action stuff

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,33 @@
-// this is the background code...
+// // this is the background code...
 
-// listen for our browserAction to be clicked
-chrome.browserAction.onClicked.addListener(function (tab) {
-  // for the current tab, inject the "inject.js" file & execute it
-  if (tab.url.indexOf('/comments/') != -1) {
-    chrome.tabs.executeScript(tab.ib, {
-        file: 'inject.js'
-	});
-  }
+// // listen for our browserAction to be clicked
+// chrome.pageAction.onClicked.addListener(function (tab) {
+//   // for the current tab, inject the "inject.js" file & execute it
+//   if (tab.url.indexOf('/comments/') != -1) {
+//     chrome.tabs.executeScript(tab.ib, {
+//         file: 'inject.js'
+// 	});
+//   }
+// });
+
+
+
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: '*.reddit.com/r/*/comments/*' },
+            console.log('on reddit page');
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.tabs.executeScript(tab.ib, {file: 'inject.js' ]});
+    ]);
+  });
 });
--- extension/options.js
+++ extension/options.js
@@ -16,6 +16,8 @@ function save_options() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
+    console.log(chrome.extension.getBackgroundPage());
+    chrome.extension.getBackgroundPage().window.location.reload();
   });
 }
 
